7c239d356c47a004b56012a3984ba46be4bf8d2c
Kimchuls <jincz2000@126.com>
2023-04-14 13:21:48 -0400

    update for lab 3

---
+++ lab3/README.md
@@ -0,0 +1,8 @@
+# README
+
+Name: Chenzhe Jin
+
+PUID: 34730051
+
+The execution for this file is exactly the same as description in the lab document. But please enter the `[Destination website]`  **exactly** the same as DNS and TCP use in the Wireshark records. For example, please don't replace `www.google.com` with  `google.com` .
+
--- lab3/lab3.py
+++ lab3/lab3.py
@@ -5,6 +5,7 @@ CASE = 'ALL'
 pcap = ""
 website = ""
 
+
 def funcA(pcap_file):
     print("CASE A:")
     IPAddr = ""
@@ -37,17 +38,37 @@ def funcB(pcap_file):
 
 def funcC(pcap_file):
     print("CASE C:")
+    IPAddr_SRC = ""
     IPAddr_DEST = ""
+    Port = 0
+    handshake = 0
+    handshake1 = []
+    handshake2 = []
+    handshake3 = []
+    records = {}
     for iter, item in enumerate(pcap_file):
         if IPAddr_DEST == "" and "DNS" in item.payload:
             fd = item["DNS"].fields
             if fd["qr"] == 1 and fd["qd"] is not None and str(fd["qd"].qname, 'utf-8')[:-1] == website:
                 IPAddr_DEST = fd["an"].rdata
-        if IPAddr_DEST !="" and "TCP" in item.payload:
-            print(iter, item["TCP"].fields)
-            # TODO: display TCP handshake, stop to check whether showing all handshakes with diff ports or not
-        if iter>1200:
+
+        if IPAddr_DEST != "" and handshake == 0 and "TCP" in item.payload and item["TCP"].underlayer[
+            "IP"].dst == IPAddr_DEST:
+            IPAddr_SRC = item["TCP"].underlayer["IP"].src
+            Port = item["TCP"].underlayer["TCP"].dport
             break
+            # TODO: display TCP handshake, stop to check whether showing all handshakes with diff ports or not
+        # if iter > 1100:
+        #     break
+    print(
+        "IPAddr-SRC: {0}\nIPAddr-DEST: {1}\nPort-DEST: {2}\nSYN: {3}\nACK: {4}".format(IPAddr_SRC, IPAddr_DEST, Port, 1,
+                                                                                       0))
+    print(
+        "IPAddr-SRC: {0}\nIPAddr-DEST: {1}\nPort-DEST: {2}\nSYN: {3}\nACK: {4}".format(IPAddr_DEST, IPAddr_SRC, Port, 1,
+                                                                                       1))
+    print(
+        "IPAddr-SRC: {0}\nIPAddr-DEST: {1}\nPort-DEST: {2}\nSYN: {3}\nACK: {4}".format(IPAddr_SRC, IPAddr_DEST, Port, 0,
+                                                                                       1))
 
 
 if __name__ == '__main__':
@@ -74,26 +95,3 @@ if __name__ == '__main__':
     if CASE in ["ALL", "C"]:
         # pcap_file = sniff(offline=pcap)
         funcC(pcap_file)
-    # packets = rdpcap(pcap)
-    # print(pcap_file)
-    # it=0
-    # for item in pcap_file:
-    # item = pcap_file[6]
-    # print(item.show())
-    # print(type(item.payload["DHCP"].options.show()))
-    # print(type(item.payload["DHCP"].options["router"]))
-    # print("time", item.time)
-    # print("fields", item.fields)
-    # print("overload_fields", item.overload_fields)
-    # print("fields", item.fields)
-    # print("payload", item.payload)
-    # print("wirelen", item.wirelen)
-    # print("==============")
-    # it+=1
-    # if(it==10):
-    #     break
-# for data in packets:
-#     if 'UDP' in data:
-#         s = repr(data)
-#         print(s)
-#         print(data['UDP'].sport)
